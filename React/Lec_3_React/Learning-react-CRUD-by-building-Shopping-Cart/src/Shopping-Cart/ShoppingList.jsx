aa78eab6454d1d17dcf0c4553df27f72080586aa
Chandan-Yadav313 <chandanydv313@gmail.com>
2024-09-20 22:17:37 +0530

    handleAddItem

--- React/Lec_3_React/Learning-react-CRUD-by-building-Shopping-Cart/src/Shopping-Cart/ShoppingList.jsx
+++ React/Lec_3_React/Learning-react-CRUD-by-building-Shopping-Cart/src/Shopping-Cart/ShoppingList.jsx
@@ -3,14 +3,27 @@ import React, { useState } from 'react'
 function ShoppingList() {
     const [content, setContent] = useState("");
     // add list through the empty array state
-    const [tasks, setTasks] = useState(["Tea"]);
+    const [tasks, setTasks] = useState([]);
 
     const inputHandle = (event) => {
         setContent(event.target.value);
     }
 
     const handleAddItem = () => {
-        console.log("Add Item")
+        // ===============================================
+        //    input value
+        /*const newItem = content;
+        // add item to the array
+        tasks.push(newItem);
+        // empty input box or content
+        setContent("");*/
+        // ====================================================
+        //    input value
+        const newItem = content;
+        // add item to the array
+        tasks.push(newItem);
+        // empty input box or content
+        setContent("");
     }
 
     const handleRemove = () => {
@@ -23,7 +36,7 @@ function ShoppingList() {
                 <input type="text" value={content} onChange={inputHandle} />
                 <button onClick={handleAddItem}>Add Item</button>
             </div>
-            
+
             <div className='shopping-list'>
                 <ul className='list'>
                     {tasks.map((item) => {
@@ -36,7 +49,6 @@ function ShoppingList() {
                 </ul>
             </div>
         </div>
-
 
     )
 }
